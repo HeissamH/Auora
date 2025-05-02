78f11d49e33ec267d5c794e4d2c91422d4e39dfd
hebron <hebron@kangtheslayer.hebron.com>
2025-05-02 15:56:41 +0300

    i added a latest movies section

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,4 +1,3 @@
-
 import { icons } from "@/constants/icons";
 import { images } from "@/constants/images";
 import { Text, View, Image, ScrollView, ActivityIndicator } from "react-native";
@@ -8,25 +7,35 @@ import { fetchMovies } from "@/services/api";
 import useFetch from "@/services/useFetch";
 
 export default function Index() {
-
   const router = useRouter();
+  const { data: movies, loading: moviesLoading, error: moviesError } = useFetch(() => fetchMovies({ query: '' }));
 
   return (
-    <View className="flex-1 bg-dark-200">  
-<Image source={images.bg} className=" absolute w-full z-0"/>
-
-<ScrollView className="flex-1 px-7" showsVerticalScrollIndicator={false} contentContainerStyle={{ paddingBottom: 10, minHeight: "100%" }}>
-<Image source={icons.logo} className="w-12 h-10 mt-20 mb-9 mx-auto"/> 
-<Text className="text-white text-3xl font-bold text-center mt-1 ml-2 mr-7 mb-5">Auora</Text>
- 
- 
-
-<SearchBar
-       placeholder="Search Movies"
-       onPress={() => router.push("/search")}
-                />
-                
-</ScrollView>
+    <View className="flex-1 bg-dark-200">
+      <Image source={images.bg} className="absolute w-full z-0" />
+      <ScrollView 
+        className="flex-1 px-7" 
+        showsVerticalScrollIndicator={false} 
+        contentContainerStyle={{ paddingBottom: 10, minHeight: "100%" }}
+      >
+        <Image source={icons.logo} className="w-12 h-10 mt-20 mb-9 mx-auto" />
+        <Text className="text-white text-3xl font-bold text-center mt-1 ml-2 mr-7 mb-5">
+          Auora
+        </Text>
+        {moviesLoading ? (
+          <ActivityIndicator size="large" color="#fff" className="mt-10" />
+        )  : (
+          <View className="flex-1 mt-5">
+            <SearchBar
+              placeholder="Search for movies"
+              onPress={() => router.push("/search")}
+            />
+            <Text className="text-lg text-white font-bold mt-5 mb-3">
+              Latest Movies
+            </Text>
+          </View>
+        )}
+      </ScrollView>
     </View>
   );
 }
